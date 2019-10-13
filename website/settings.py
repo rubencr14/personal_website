eea3067ebc9e87a7a289ca459b1c242538a208ff
ruben cañadas rodriguez <rubencr@MacBook-Pro-de-ruben.local>
2019-10-13 23:21:57 +0200

    no auth

--- website/settings.py
+++ website/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 #STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-#STATIC_ROOT = 'static' #for heroku
+STATIC_ROOT = 'static' #for heroku
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 #STATIC_ROOT = 'staticfiles'
